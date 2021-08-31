4dd8ed343f17510e8b0dbea2b313d98454dbcc52
Namgyung2 <79958830+Namgyung2@users.noreply.github.com>
2021-08-31 19:41:54 +0900

    Add store

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore } from 'vuex'
+import product from './product.js'
+
+export default createStore({
+  modules: {
+    product
+  }
+})
---
+++ src/store/product.js
@@ -0,0 +1,15 @@
+export default {
+  //module
+  namespaced: true,
+  //data
+  state: ()=>({
+    con: []
+  }),
+  //computed (계산된 데이터)
+  getters:{
+    
+  },
+  //methods 
+  mutations:'',
+  actions:''
+}
